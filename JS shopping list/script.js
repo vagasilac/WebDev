2ceb28f70eb17ba2f2b066c84e433260136876ba
vagasilac <vagasilac@gmail.com>
2022-08-23 10:23:49 +0300

    Update script.js
    I included the explanations that I found on the web for parts that were not clear. e.g. event.target

--- JS shopping list/script.js
+++ JS shopping list/script.js
@@ -20,11 +20,14 @@ function createListElement() {
 // CREATE AN ELEMENT
 	var button =document.createElement("button"); 
 // APPEND TEXT TO BUTTON
-	button.appendChild(document.createTextNode("Delete!"));
+	button.appendChild(document.createTextNode("Delete!")); /* [vagasilac]: This new button won't have the "enter" id,
+	so there's no confusion. */
 // APPEND BUTTON TO LIST
 	li.appendChild(button);
 
-	button.onclick=removeParent;
+	button.onclick=removeParent; /* [vagasilac]: This is the part of the solution that I could not find out on my own... to call the
+	remove-function with "onclick" */
+
 //THIS FUNCTION WILL DO THIS FOR EVERY BUTTON CREATED.
 	
 }
@@ -34,9 +37,34 @@ function createListElement() {
 // 	target.classList.toggle("done");
 // }
 
-function underlineParent(event){
+function underlineParent(event){		/* [vagasilac]: Here, I'll need to understand how this "cross-referencing" of
+|event -> event| or |evt -> evt| below works...   also "target" */
 	event.target.parentNode.classList.toggle("done");
 }
+
+/* Here's a nice explanation for EVENT.TARGET:
+
+// Make a list
+const ul = document.createElement('ul');
+document.body.appendChild(ul);
+
+const li1 = document.createElement('li');
+const li2 = document.createElement('li');
+ul.appendChild(li1);
+ul.appendChild(li2);
+
+function hide(evt) {
+  // evt.target refers to the clicked <li> element
+  // This is different than evt.currentTarget, which would refer to the parent <ul> in this context
+  evt.target.style.visibility = 'hidden';
+}
+
+// Attach the listener to the list
+// It will fire when each <li> is clicked
+ul.addEventListener('click', hide, false);
+
+*/
+
 
 
 function removeParent(evt){
